b673c1f138d741fabe068eb57303c261cd037903
Helder Burato Berto <helder.burato@gmail.com>
2019-10-17 21:11:18 -0300

    Added Http status codes

---
+++ src/constants/http-statuses.js
@@ -0,0 +1,386 @@
+export default [
+  {
+    code: 100,
+    key: 'CONTINUE',
+    message: 'Continue',
+    category: 'INFORMATIONAL'
+  },
+  {
+    code: 101,
+    key: 'SWITCHING_PROTOCOLS',
+    message: 'Switching Protocols',
+    category: 'INFORMATIONAL'
+  },
+  {
+    code: 102,
+    key: 'PROCESSING',
+    message: 'Processing',
+    category: 'INFORMATIONAL'
+  },
+  {
+    code: 200,
+    key: 'OK',
+    message: 'OK',
+    category: 'SUCCESS'
+  },
+  {
+    code: 201,
+    key: 'CREATED',
+    message: 'Created',
+    category: 'SUCCESS'
+  },
+  {
+    code: 202,
+    key: 'ACCEPTED',
+    message: 'Accepted',
+    category: 'SUCCESS'
+  },
+  {
+    code: 203,
+    key: 'NON_AUTHORITATIVE_INFORMATION',
+    message: 'Non-authoritative Information',
+    category: 'SUCCESS'
+  },
+  {
+    code: 204,
+    key: 'NO_CONTENT',
+    message: 'No Content',
+    category: 'SUCCESS'
+  },
+  {
+    code: 205,
+    key: 'RESET_CONTENT',
+    message: 'Reset Content',
+    category: 'SUCCESS'
+  },
+  {
+    code: 205,
+    key: 'RESET_CONTENT',
+    message: 'Reset Content',
+    category: 'SUCCESS'
+  },
+  {
+    code: 206,
+    key: 'PARTIAL_CONTENT',
+    message: 'Partial Content',
+    category: 'SUCCESS'
+  },
+  {
+    code: 207,
+    key: 'MULTI_STATUS',
+    message: 'Multi-Status',
+    category: 'SUCCESS'
+  },
+  {
+    code: 208,
+    key: 'ALREADY_REPORTED',
+    message: 'Already Reported',
+    category: 'SUCCESS'
+  },
+  {
+    code: 216,
+    key: 'IM_USED',
+    message: 'IM Used',
+    category: 'SUCCESS'
+  },
+  {
+    code: 300,
+    key: 'MULTIPLE_CHOICES',
+    message: 'Multiple Choices',
+    category: 'REDIRECTION'
+  },
+  {
+    code: 301,
+    key: 'MOVED_PERMANENTLY',
+    message: 'Moved Permanently',
+    category: 'REDIRECTION'
+  },
+  {
+    code: 302,
+    key: 'FOUND',
+    message: 'Found',
+    category: 'REDIRECTION'
+  },
+  {
+    code: 303,
+    key: 'SEE_OTHER',
+    message: 'See Other',
+    category: 'REDIRECTION'
+  },
+  {
+    code: 304,
+    key: 'NOT_MODIFIED',
+    message: 'Not Modified',
+    category: 'REDIRECTION'
+  },
+  {
+    code: 305,
+    key: 'USE_PROXY',
+    message: 'Use Proxy',
+    category: 'REDIRECTION'
+  },
+  {
+    code: 307,
+    key: 'TEMPORARY_REDIRECT',
+    message: 'Temporary Redirect',
+    category: 'REDIRECTION'
+  },
+  {
+    code: 308,
+    key: 'PERMANENT_REDIRECT',
+    message: 'Permanent Redirect',
+    category: 'REDIRECTION'
+  },
+  {
+    code: 400,
+    key: 'BAD_REQUEST',
+    message: 'Bad Request',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 401,
+    key: 'UNAUTHORIZED',
+    message: 'Unauthorized',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 402,
+    key: 'PAYMENT_REQUIRED',
+    message: 'Payment Required',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 403,
+    key: 'FORBIDDEN',
+    message: 'Forbidden',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 404,
+    key: 'NOT_FOUND',
+    message: 'Not Found',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 405,
+    key: 'METHOD_NOT_ALLOWED',
+    message: 'Method Not Allowed',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 406,
+    key: 'NOT_ACCEPTABLE',
+    message: 'Not Acceptable',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 407,
+    key: 'PROXY_AUTHENTICATION_REQUIRED',
+    message: 'Proxy Authentication Required',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 408,
+    key: 'REQUEST_TIMEOUT',
+    message: 'Request Timeout',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 409,
+    key: 'CONFLICT',
+    message: 'Conflict',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 410,
+    key: 'GONE',
+    message: 'Gone',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 411,
+    key: 'LENGTH_REQUIRED',
+    message: 'Length Required',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 412,
+    key: 'PRECONDITION_FAILED',
+    message: 'Precondition Failed',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 413,
+    key: 'PAYLOAD_TOO_LARGE',
+    message: 'Payload Too Large',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 414,
+    key: 'REQUEST_URI_TOO_LONG',
+    message: 'Request-URI Too Long',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 415,
+    key: 'UNSUPORTED_MEDIA_TYPE',
+    message: 'Unsupported Media Type',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 416,
+    key: 'REQUESTED_RANGE_NOT_SATISFIABLE',
+    message: 'Requested Range Not Satisfiable',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 417,
+    key: 'EXPECTATION_FAILED',
+    message: 'Expectation Failed',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 418,
+    key: 'IM_A_TEAPOT',
+    message: "I'm a teapot",
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 421,
+    key: 'MISDIRECTED_REQUEST',
+    message: 'Misdirected Request',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 422,
+    key: 'UNPROCESSABLE_ENTITY',
+    message: 'Unprocessable Entity',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 423,
+    key: 'LOCKED',
+    message: 'Locked',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 424,
+    key: 'FAILED_DEPENDENCY',
+    message: 'Failed Dependency',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 426,
+    key: 'UPGRADE_REQUIRED',
+    message: 'Upgrade Required',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 428,
+    key: 'PRECONDITION_REQUIRED',
+    message: 'Precondition Required',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 429,
+    key: 'TOO_MANY_REQUESTS',
+    message: 'Too Many Requests',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 431,
+    key: 'REQUEST_HEADER_FIELDS_TOO_LARGE',
+    message: 'Request Header Fields Too Large',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 444,
+    key: 'CONNECTION_CLOSED_WITHOUT_RESPONSE',
+    message: 'Connection Closed Without Response',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 451,
+    key: 'UNAVAILABLE_FOR_LEGAL_REASONS',
+    message: 'Unavailable For Legal Reasons',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 499,
+    key: 'CLIENT_CLOSED_REQUEST',
+    message: 'Client Closed Request',
+    category: 'CLIENT_ERROR'
+  },
+  {
+    code: 500,
+    key: 'INTERNAL_SERVER_ERROR',
+    message: 'Internal Server Error',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 501,
+    key: 'NOT_IMPLEMENTED',
+    message: 'Not Implemented',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 502,
+    key: 'BAD_GATEWAY',
+    message: 'Bad Gateway',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 503,
+    key: 'SERVICE_UNAVAILABLE',
+    message: 'Service Unavailable',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 504,
+    key: 'GATEWAY_TIMEOUT',
+    message: 'Gateway Timeout',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 505,
+    key: 'HTTP_VERSION_NOT_SUPPORTED',
+    message: 'HTTP Version Not Supported',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 506,
+    key: 'VARIANT_ALSO_NEGOTIATES',
+    message: 'Variant Also Negotiates',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 507,
+    key: 'INSUFFICIENT_STORAGE',
+    message: 'Insufficient Storage',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 508,
+    key: 'LOOP_DETECTED',
+    message: 'Loop Detected',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 510,
+    key: 'NOT_EXTENDED',
+    message: 'Not Extended',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 511,
+    key: 'NETWORK_AUTHENTICATION_REQUIRED',
+    message: 'Network Authentication Required',
+    category: 'SERVER_ERROR'
+  },
+  {
+    code: 599,
+    key: 'NETWORK_CONNECT_TIMEOUT_ERROR',
+    message: 'Network Connect Timeout Error',
+    category: 'SERVER_ERROR'
+  }
+];
