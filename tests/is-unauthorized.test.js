59fe538853ad44cbcc1fb2565cf82da018bdf4b7
Helder Burato Berto <helder.burato@gmail.com>
2019-10-20 22:51:05 -0300

    feat: isUnauthorized validation

---
+++ tests/is-unauthorized.test.js
@@ -0,0 +1,12 @@
+import { isUnauthorized } from '../src';
+
+describe('isMovedPermanently', () => {
+  test('it should receive true when pass status 401', () => {
+    expect(isUnauthorized(401)).toBeTruthy();
+  });
+
+  test('it should throw Error when pass status different than 401', () => {
+    const received = isUnauthorized(500);
+    expect(received.message).toBe('Expected a 401 response.');
+  });
+});
