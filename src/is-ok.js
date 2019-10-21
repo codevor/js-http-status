a85839110d41887582a370d6e31753e8353f83e9
Helder Burato Berto <helder.burato@gmail.com>
2019-10-20 21:54:34 -0300

    refactor: isOk method add params to documentation and tests messages to better readable

--- src/is-ok.js
+++ src/is-ok.js
@@ -1,5 +1,14 @@
-function isOk(status) {
-  return status === 200;
+import validateHttpStatus from './utils/validate-http-status';
+
+/**
+ * Validate HTTP Status code 200 type SUCCESS
+ *
+ * @param {integer} statusCode - The HTTP Status code
+ * @throws {HTTPStatusError} Will throw an error if the @param {statusCode} is different then 200
+ * @return {boolean}
+ */
+function isOk(statusCode) {
+  return validateHttpStatus(statusCode, 200);
 }
 
 export default isOk;
