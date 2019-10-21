2541749d3b16a8473282a419c19c5c9f07e67e78
Helder Burato Berto <helder.burato@gmail.com>
2019-10-20 21:52:25 -0300

    refactor: load packages from directory utils

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as isCreated } from './is-created';
 export { default as isOk } from './is-ok';
 export { default as findHttpStatus } from './find-http-status';
-export { default as validateHttpStatus } from './validate-http-status';
+export { default as validateHttpStatus } from './utils/validate-http-status';
+export { default as HTTPStatusError } from './utils/http-status-error';
